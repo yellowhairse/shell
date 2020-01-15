a3e8690324e207bfc755c90253572b9f95bcfe04
yellowhairse <54492161+yellowhairse@users.noreply.github.com>
2020-01-15 17:33:43 +0800

    Initial commit

---
+++ test/001.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+#LOG_DIR='/c/Users/wtong/Documents/shell/.idea/log'
+#
+#cd ${LOG_DIR}
+#
+#echo "The # here does not begin a comment."
+#echo 'The # here does not begin a comment.'
+#echo The \# here does not begin a comment.
+#echo The # 这里开始一个注释
+#
+#echo ${PATH#*:} # 参数替换,不是一个注释
+#echo $(( 2#101011 )) # 数制转换,不是一个注释
+
+# Thanks, S.C.
+
+#echo hello; echo there
+#if [ -x "$filename" ]; then # 注意:"if"和"then"需要分隔
+## 为啥?
+#echo "File $filename exists."; cp $filename $filename.bak
+#else
+#echo "File $filename not found."; touch $filename
+#fi; echo "File test complete."
+
+
+#echo "我">>ghostwu.sh
+
+
+while read line
+do
+echo $line
+done < ghostwu.sh
+
+
+
+
+
+
+
+
