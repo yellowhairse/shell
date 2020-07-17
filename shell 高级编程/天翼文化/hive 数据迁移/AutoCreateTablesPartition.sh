6c1a34d96fe50b77e1c076d1f401b234e817f213
Administrator <yellowhairse>
2020-07-17 10:45:47 +0800

    more better

--- shell 高级编程/天翼文化/hive 数据迁移/AutoCreateTablesPartition.sh
+++ shell 高级编程/天翼文化/hive 数据迁移/AutoCreateTablesPartition.sh
@@ -2,39 +2,38 @@
 
 # -*- coding: utf-8 -*-
 # @Time    : 2020/7/16 13:19
-# @Author  : Administrator
+# @Author  : wangtong
 # @Site    : 为hive 中表增加未来7天的分区
-# @File    : ${PACKAGE_NAME} ${NAME}
-# @Software: IntelliJ IDEA
+# @File    :
+# @Software:
 
+#定义参数值
+#需要指定表名，分区路径的顺序对应关系；需要建分区的表的个数
 
-#定义最近8天 格式：yyyymmdd
-currDate=`date +%Y%m%d`
-currDate1=`date +%Y%m%d -d "+1 day"`
-currDate2=`date +%Y%m%d -d "+2 day"`
-currDate3=`date +%Y%m%d -d "+3 day"`
-currDate4=`date +%Y%m%d -d "+4 day"`
-currDate5=`date +%Y%m%d -d "+5 day"`
-currDate6=`date +%Y%m%d -d "+6 day"`
-currDate7=`date +%Y%m%d -d "+7 day"`
-Datelist=($currDate $currDate1 $currDate2 $currDate3 $currDate4 $currDate5 $currDate6 $currDate7 )
-
-#定义需要建分区的表
-#需要指定表名；和分区路径
 tablename=('ods.ods_kafka_topic_log_file' 'ods_test.ods_kafka_topic_log_file')
 hdfsurl='hdfs://tyvrdwcmnnprod01.189read.com:8020'
 hdfspath=('/user/hive/warehouse/ods/vrlog/ods_vrlog_topic_log_file/' '/user/hive/warehouse/ods_test/vrlog/ods_vrlog_topic_log_file/')
-count=0
-#echo ${tablename[0]}
 
+#返回8天的数组 格式：yyyymmdd
+function Getdatelist(){
+    Datelist=()
+    currDate=`date +%Y%m%d`
+    for count in {0..7}
+    do
+    Datelist[$count]=`date +%Y%m%d -d "+${count} day"`
+    done
+    echo ${Datelist[@]}
+}
+
+#建HIVE分区语句封装
 function CreateHivePar(){
     #`hive -e 'alter table '$1' add IF NOT EXISTS partition(dt='\'${2}\'') location '\'${hdfsurl}${3}dt=${2}/\'`
     echo 'alter table '$1' add IF NOT EXISTS partition(dt='\'${2}\'') location '\'${hdfsurl}${3}dt=${2}/\'
 }
 
-
+#根据数组中时间范围建HIVE分区
 function CreatePartitionMore(){
-for d in ${Datelist[@]}
+for d in `Getdatelist`
 do
     CreateHivePar ${tablename[$1]} ${d} ${hdfspath[$1]}
     if [ $? -eq 0 ];
@@ -45,7 +44,10 @@ do
 done
 }
 
-CreatePartitionMore 0
-
+#0..1 代表2张表；添加表后要做调整
+for i in {0..1}
+do
+    CreatePartitionMore ${i}
+done
 
 
---
+++ shell 高级编程/天翼文化/hive 数据迁移/RunAutoCreateTablesPartition.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+    
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/17 10:35
+# @Author  : Administrator
+# @Site    : 运行建HIVE分区脚本
+# @File    : ${PACKAGE_NAME} ${NAME} 
+# @Software: IntelliJ IDEA
+
+#定义路径和脚本
+Scriptpath='/home/bigdata/hive/Script_shell/'
+Scriptnote='AutoCreateTablesPartition.sh'
+
+#执行脚本
+nohup sh ${Scriptpath}${Scriptnote} &
+
+
+/home/bigdata/hive/Run/RunAutoCreateTablesPartition.sh
